b12f9120dd18ec666062018ebb39880504de6166
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2020-09-19 19:39:47 +0530

    added quick sort

--- Sorting-Algs.py
+++ Sorting-Algs.py
@@ -1,3 +1,13 @@
+def choice(arr):
+    print("Enter the sort you want to perform: ")
+    print("1.Insertion Sort\n2.Quick Sort")
+    c = int(input("Enter you choice: "))
+
+    if c == 1:
+        return insertion_sort(arr)
+    if c == 2:
+        return quick_sort(arr, 0, n-1)
+
 def insertion_sort(arr):
     for x in range(1, len(arr)):
         buffer = arr[x]
@@ -9,19 +19,40 @@ def insertion_sort(arr):
     return arr
 
 
-def quick_sort(arr):
+def quick_sort(arr,low,high):
+    def partition(arr,low,high): 
+        i = ( low-1 )         # index of smaller element 
+        pivot = arr[high]     # pivot 
+    
+        for j in range(low , high): 
+    
+            # If current element is smaller than the pivot 
+            if   arr[j] < pivot: 
+            
+                # increment index of smaller element 
+                i = i+1 
+                arr[i],arr[j] = arr[j],arr[i] 
+    
+        arr[i+1],arr[high] = arr[high],arr[i+1] 
+        return ( i+1 ) 
+
+    if low < high: 
+
+        # pi is partitioning index, arr[p] is now 
+        # at right place 
+        pi = partition(arr,low,high) 
+
+        # Separately sort elements before 
+        # partition and after partition 
+        quick_sort(arr, low, pi-1) 
+        quick_sort(arr, pi+1, high)
 
     return arr
 
-def choice(arr):
-    print("Enter the sort you want to perform: ")
-    print("1.Insertion Sort\n2.Quick Sort")
-    c = int(input("Enter you choice: "))
-
-    if c == 1:
-        return insertion_sort(arr)
-    if c == 2:
-        quick_sort(arr)
-
 MyList = list(map(int,input("\nEnter the Numbers : ").strip().split()))
-print(choice(MyList))
+n = len(MyList)
+
+SortedList = choice(MyList)
+
+for x in SortedList:
+    print(x,end=" ")
