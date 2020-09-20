90e213e90fdb25946781910c78cd15f9f2be17c3
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2020-09-20 14:26:12 +0530

    added selection sort

--- Sorting-Algs.py
+++ Sorting-Algs.py
@@ -1,13 +1,19 @@
+# Choosing the Sorting Algorithm
 def choice(arr):
     print("Enter the sort you want to perform: ")
-    print("1.Insertion Sort\n2.Quick Sort")
+    print("1.Insertion Sort")
+    print("2.Quick Sort")
+    print("3.Selection Sort")
     c = int(input("Enter you choice: "))
 
     if c == 1:
         return insertion_sort(arr)
     if c == 2:
         return quick_sort(arr, 0, n-1)
+    if c == 3:
+        return selection_sort(arr)
 
+# Insertion Sort
 def insertion_sort(arr):
     for x in range(1, len(arr)):
         buffer = arr[x]
@@ -18,8 +24,9 @@ def insertion_sort(arr):
             j -= 1
     return arr
 
-
+# Quick Sort
 def quick_sort(arr,low,high):
+
     def partition(arr,low,high): 
         i = ( low-1 )         
         pivot = arr[high]     
@@ -43,10 +50,24 @@ def quick_sort(arr,low,high):
 
     return arr
 
+# Selection Sort
+def selection_sort(arr):
+    
+    for x in range(len(arr)):
+        min = x
+        for y in range(x+1, len(arr)):
+            if arr[min] > arr[y]:
+                min = y
+        arr[x], arr[min] = arr[min], arr[x]
+    return arr
+
+# Inputing the List/Array
 MyList = list(map(int,input("\nEnter the Numbers : ").strip().split()))
+
 n = len(MyList)
 
 SortedList = choice(MyList)
 
+# Printing the Sorted List/Array
 for x in SortedList:
     print(x,end=" ")
