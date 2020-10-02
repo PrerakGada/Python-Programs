178516c1e293fa262174901f6408d95ee1c57d37
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2020-10-03 02:23:40 +0530

    searching algs

---
+++ Searching-Algs.py
@@ -0,0 +1,11 @@
+def choice():
+    print("1.Linear Search")
+    print("1.Binary Search")
+
+    c = int(input("Enter your choice: "))
+
+    if c == 1:
+        linearsearch()
+
+def linearsearch():
+    pass
