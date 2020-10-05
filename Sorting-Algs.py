d3fd2f9312893ffbb69776b86a3b8729430a6836
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2020-10-05 21:19:55 +0530

    Merge Sort

--- Sorting-Algs.py
+++ Sorting-Algs.py
@@ -5,6 +5,7 @@ def choice():
     print("2.Quick Sort")
     print("3.Selection Sort")
     print("4.Bubble Sort")
+    print("5.Merge Sort")
     c = int(input("Enter your choice: "))
 
 
@@ -16,6 +17,8 @@ def choice():
         return selection_sort(MyList)
     elif c == 4:
         return bubble_sort(MyList)
+    elif c == 5:
+        return merge_sort(MyList)
     else:
         print("Invalid Choice!!")
 
@@ -76,6 +79,38 @@ def bubble_sort(arr):
                 arr[y], arr[y+1] = arr[y+1], arr[y]
     return arr
 
+# Merge Sort
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right): 
+            if left[i] < right[j]: 
+                arr[k] = left[i] 
+                i+= 1
+            else: 
+                arr[k] = right[j] 
+                j+= 1
+            k+= 1
+
+        while i < len(left): 
+            arr[k] = left[i] 
+            i+= 1
+            k+= 1
+          
+        while j < len(right): 
+            arr[k] = right[j] 
+            j+= 1
+            k+= 1
+
+    return arr
 
 
 # Inputing the List/Array
