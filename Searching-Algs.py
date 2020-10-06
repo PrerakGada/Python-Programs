f101cfd5dee82bb40f677e1bb061aa81a5360922
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2020-10-06 13:29:12 +0530

    Linear Search

--- Searching-Algs.py
+++ Searching-Algs.py
@@ -1,18 +1,30 @@
-def choice():
+def choice(arr, x):
     print("1.Linear Search")
-    print("1.Binary Search")
+    print("2.Binary Search")
 
     c = int(input("Enter your choice: "))
 
     if c == 1:
-        linearsearch()
+        linearsearch(arr, x)
+    elif c == 2:
+        binarysearch(arr, x)
+    else:
+        print("Wrong Choice!")
+        choice(arr, x)
 
-def linearsearch():
-    pass
+def linearsearch(arr, x):
 
-def binarysearch():
+    for i in range(len(arr)):
+        if arr[i] == x:
+            print("{0} is Found on {1} Location",format(x,i))
+
+def binarysearch(arr, x):
+    arr.sort()
+
     pass
 
 MyList = list(map(int,input("\nEnter the Numbers : ").strip().split()))
 
 num = int(input("Enter the Number you want to Search: "))
+
+choice(MyList, num)
