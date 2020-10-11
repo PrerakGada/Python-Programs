fe091c085ba9a8ff3787f517bb9ba6cf8028cbfb
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2020-10-11 20:39:20 +0530

    update

--- Sorting-Algs.py
+++ Sorting-Algs.py
@@ -94,16 +94,16 @@ def merge_sort(arr):
         while i < len(left) and j < len(right): 
             if left[i] < right[j]: 
                 arr[k] = left[i] 
-                i+= 1
+                i += 1
             else: 
                 arr[k] = right[j] 
-                j+= 1
-            k+= 1
+                j += 1
+            k += 1
 
         while i < len(left): 
             arr[k] = left[i] 
-            i+= 1
-            k+= 1
+            i += 1
+            k += 1
           
         while j < len(right): 
             arr[k] = right[j] 
