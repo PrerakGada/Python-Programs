6340d77102d6f8ba4a9df2e505b255b55ef1273a
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2020-10-19 00:30:24 +0530

    oop practice

--- Practice.py
+++ Practice.py
@@ -1,5 +1,18 @@
+class Computer:
 
-file = open('text.txt', 'r')
-x = file.read()
-print(x)
+    def __init__(self):
+        self.name = 'Prerak'
+        self.age = 17
+        
+    def update(self):
+        self.name = 'Ghost'
+        self.age = 18 
 
+c1 = Computer()
+c2 = Computer()
+
+print(c1.name)
+print(c1.age)
+c2.update()
+print(c2.name)
+print(c2.age)
