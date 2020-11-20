ed0a9155273877000b92c879e115073f6ee395ff
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2020-11-21 02:32:47 +0530

    Added Binary Search

--- Searching-Algs.py
+++ Searching-Algs.py
@@ -12,21 +12,36 @@ def choice(arr, x):
         print("Wrong Choice!")
         choice(arr, x)
 
+
 def linearsearch(arr, x):
     o = 0
     for i in range(len(arr)):
         if arr[i] == x:
-            print("{0} is Found on {1} Location".format(x,i))
+            print("{0} is Found on {1} Location".format(x, i))
             o += 1
     if o == 0:
         print("{0} is not Found in the List!".format(x))
 
+
 def binarysearch(arr, x):
     arr.sort()
+    min = 0
+    max = len(arr) - 1
+    mid = (min + max) // 2
 
-    pass
+    while arr[mid] != x:
+        if x == arr[mid]:
+            break
+        elif x > arr[mid]:
+            min = mid
+            mid = (min + max) // 2
+        elif x < arr[mid]:
+            max = mid
+            mid = (min + max) // 2
+    print("{0} is Found at {1} Location".format(x, arr[mid]))
 
-MyList = list(map(int,input("\nEnter the Numbers : ").strip().split()))
+
+MyList = list(map(int, input("\nEnter the Numbers : ").strip().split()))
 
 num = int(input("Enter the Number you want to Search: "))
 
--- Sorting-Algs.py
+++ Sorting-Algs.py
@@ -8,11 +8,10 @@ def choice():
     print("5.Merge Sort")
     c = int(input("Enter your choice: "))
 
-
     if c == 1:
         return insertion_sort(MyList)
     elif c == 2:
-        return quick_sort(MyList, 0, n-1)
+        return quick_sort(MyList, 0, n - 1)
     elif c == 3:
         return selection_sort(MyList)
     elif c == 4:
@@ -21,6 +20,7 @@ def choice():
         return merge_sort(MyList)
     else:
         print("Invalid Choice!!")
+
 
 # Insertion Sort
 def insertion_sort(arr):
@@ -33,56 +33,55 @@ def insertion_sort(arr):
             j -= 1
     return arr
 
+
 # Quick Sort
-def quick_sort(arr,low,high):
+def quick_sort(arr, low, high):
+    def partition(arr, low, high):
+        i = (low - 1)
+        pivot = arr[high]
 
-    def partition(arr,low,high): 
-        i = ( low-1 )         
-        pivot = arr[high]     
-    
-        for j in range(low , high): 
-    
-            if   arr[j] < pivot: 
-            
-                i = i+1 
-                arr[i],arr[j] = arr[j],arr[i] 
-    
-        arr[i+1],arr[high] = arr[high],arr[i+1] 
-        return i + 1 
+        for j in range(low, high):
 
-    if low < high: 
+            if arr[j] < pivot:
+                i = i + 1
+                arr[i], arr[j] = arr[j], arr[i]
 
-        pi = partition(arr,low,high) 
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+        return i + 1
 
-        quick_sort(arr, low, pi-1) 
-        quick_sort(arr, pi+1, high)
+    if low < high:
+        pi = partition(arr, low, high)
+
+        quick_sort(arr, low, pi - 1)
+        quick_sort(arr, pi + 1, high)
 
     return arr
 
+
 # Selection Sort
 def selection_sort(arr):
-    
     for x in range(len(arr)):
         min = x
-        for y in range(x+1, len(arr)):
+        for y in range(x + 1, len(arr)):
             if arr[min] > arr[y]:
                 min = y
         arr[x], arr[min] = arr[min], arr[x]
     return arr
 
-# Bubble Sort
-def bubble_sort(arr): 
 
-    for x in range(len(arr)): 
-        for y in range(0, len(arr)-x-1): 
-            if arr[y] > arr[y+1] : 
-                arr[y], arr[y+1] = arr[y+1], arr[y]
+# Bubble Sort
+def bubble_sort(arr):
+    for x in range(len(arr)):
+        for y in range(0, len(arr) - x - 1):
+            if arr[y] > arr[y + 1]:
+                arr[y], arr[y + 1] = arr[y + 1], arr[y]
     return arr
+
 
 # Merge Sort
 def merge_sort(arr):
     if len(arr) > 1:
-        mid = len(arr)//2
+        mid = len(arr) // 2
         left = arr[:mid]
         right = arr[mid:]
 
@@ -91,22 +90,22 @@ def merge_sort(arr):
 
         i = j = k = 0
 
-        while i < len(left) and j < len(right): 
-            if left[i] < right[j]: 
-                arr[k] = left[i] 
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
                 i += 1
-            else: 
-                arr[k] = right[j] 
+            else:
+                arr[k] = right[j]
                 j += 1
             k += 1
 
-        while i < len(left): 
-            arr[k] = left[i] 
+        while i < len(left):
+            arr[k] = left[i]
             i += 1
             k += 1
-          
-        while j < len(right): 
-            arr[k] = right[j] 
+
+        while j < len(right):
+            arr[k] = right[j]
             j += 1
             k += 1
 
@@ -114,7 +113,7 @@ def merge_sort(arr):
 
 
 # Inputing the List/Array
-MyList = list(map(int,input("\nEnter the Numbers : ").strip().split()))
+MyList = list(map(int, input("\nEnter the Numbers : ").strip().split()))
 
 n = len(MyList)
 
@@ -122,4 +121,4 @@ SortedList = choice()
 
 # Printing the Sorted List/Array
 for x in SortedList:
-    print(x,end=" ")
+    print(x, end=" ")
