3a7bea8da3c4e60e88b5938b42e6f07248f3093f
Prerak Gada <prerakgada07@gmail.com>
2020-12-28 09:34:45 +0530

    string methods

---
+++ Strings.py
@@ -0,0 +1,18 @@
+first_name, last_name = ' prerak', 'gada '
+full_name = first_name + "\t" + last_name
+
+full_name.strip()
+print(full_name)
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+print(full_name.capitalize())
+print(full_name.center(20))
+print(full_name.expandtabs(5))
+print(full_name.swapcase())
+print(full_name)
+print(full_name)
+print(full_name)
+print(full_name)
+print(full_name)
+
